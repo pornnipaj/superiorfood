13da8eb61e2d3fcbd21cf5ba03261d624c6f23da
pornnipaj <33538593+pornnipaj@users.noreply.github.com>
2020-01-22 19:40:24 +0700

    commit

--- src/app/post-data.service.ts
+++ src/app/post-data.service.ts
@@ -8,8 +8,8 @@ import { Http, Headers } from "@angular/http";
 export class PostDataService {
   data;
   apiLocal_url = 'http://localhost:41669';
-  // apiServer_url = 'https://cors-anywhere.herokuapp.com/http://superior2.wingplusweb.com/';
-apiServer_url = 'http://superior2.wingplusweb.com/';
+  //apiServer_url = 'https://cors-anywhere.herokuapp.com/http://superior2.wingplusweb.com/';
+ apiServer_url = 'http://superior2.wingplusweb.com/';
 //  apiServer_url = 'http://superiortest.wingplusweb.com/';
   // apiServer_url = 'https://cors-anywhere.herokuapp.com/http://superiortest.wingplusweb.com/';
 
